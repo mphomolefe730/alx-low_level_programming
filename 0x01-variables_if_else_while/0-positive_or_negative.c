fcc30320db4fa9ca95b09617f4de1dfbc6005779
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-10 05:34:45 -0800

    0test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		
+
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -27,9 +27,9 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	
+
 	else
-	{	
+	{
 		printf("%d is negative\n", n);
 	}
 
