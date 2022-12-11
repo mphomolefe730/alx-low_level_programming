b99729c108335bf649333f60a04d448376cb9bd1
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-11 04:51:10 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,18 +12,11 @@ int main(void)
 	int alphabet = 97;
 	int alphabet2 = 65;
 	/*print  charactor*/
-	while (alphabet <= 122)
+	while (alphabet2 <= 90, alphabet <= 122)
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
-
-	while (alphabet2 <= 90)
-	{
-		putchar(alphabet2);
-		alphabet++;
-	}
-
 	putchar('\n');
 	return (0);
 }
