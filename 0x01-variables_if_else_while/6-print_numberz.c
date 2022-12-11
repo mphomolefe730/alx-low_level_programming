a39cc10d29978a83065825b9fbcf34315d673c79
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-11 13:11:31 -0800

    modfied 5 and created 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,15 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  **/
 
 int main(void)
 {
 	int number = 0;
-
-	/*function*/
 	while (number < 10)
 	{
-		printf("%d", number);
+		putchar(number);
 		number++;
 	}
-		printf("\n");
 	return (0);
 }
