4667ecfa507b77fe0215acd44cf27d9d2a9f29ab
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-11 04:22:06 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	int alphabet = 97;
+	
+	/*print  charactor*/
+	while (alphabet <= 122)
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
+
