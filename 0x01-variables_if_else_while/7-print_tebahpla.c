dd6769eeebd5180e2ebe53db424c67e0f342beab
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-12 04:16:55 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int number = 122;
+
+	while (number >= 97)
+	{
+		putchar(number);
+		number--;
+	}
+
+	return (0);
+}
