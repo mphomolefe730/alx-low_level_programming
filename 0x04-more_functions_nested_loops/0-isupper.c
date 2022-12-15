9728636deb0a03b11b20388b16e5b8b8b4d492fd
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-15 11:49:48 -0800

    added discription for return

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,12 @@
  * _isupper - checks for upper case charactors
  *
  * @c: The variable the computer put
- *
+ * Return: 1 if uppercase, 0 else
  **/
 
 int _isupper(int c)
 {
-	char = c;
-
-	if (c >= 'A' || c <= 'Z')
+	if (c >= (char)'A' || c <= (char)'Z')
 	{
 		return (1);
 	}
