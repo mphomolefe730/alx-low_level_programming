cc6f46d9037f4f719a283235a38509ccdb8320b4
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-15 16:17:07 -0800

    Changes to be committed: 	modified:   4-print_most_numbers.c 	modified:   main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9 (without 2 and 4)
+ *
+ **/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 57; num++)
+	{
+		if (num != 50 || num != 52)
+		{
+		_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
+
