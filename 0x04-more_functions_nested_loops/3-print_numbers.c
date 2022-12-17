f73453f31e87d3d6fde243da3e72056459925fda
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-16 16:27:40 -0800

    Changes to be committed: 	modified:   3-print_numbers.c 	modified:   4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 
 void print_numbers(void)
 {
-	int num;
+	char num;
 
-	for (num = 48; num < 57; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		_putchar(num);
 	}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 48; num < 57; num++)
+	for (num = 48; num < 58; num++)
 	{
 		if (num != 50 || num != 52)
 		{
