f265653e610808e31c6c20ca64da7471dbc32a66
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-20 13:48:46 -0800

    Changes to be committed: 	new file:   0-reset_to_98.c 	new file:   README.md 	new file:   main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * as parameter and updates the value it point to 98
+ *
+ * @n: the varaible the system will enter
+ **/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
