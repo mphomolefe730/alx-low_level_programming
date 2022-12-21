e1bbcc3f3a8737638a40af9fadb34b8a6da7b7c9
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-20 17:23:29 -0800

    Files was not showing in testing system

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - print the length of a string
+ *
+ * @s: the variable in question
+ **/
+
+int _strlen(char *s)
+{
+	return(strlen(s))
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - print string, followed by new line to stdout
+ * @str: String variable
+ **/
+
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
