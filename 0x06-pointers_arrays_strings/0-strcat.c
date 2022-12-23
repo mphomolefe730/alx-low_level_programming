51dd1abf94095b4b75c4ae49a7a4a24b3e9375a1
mphomolefe730 <mphomolefe730@gmail.com>
2022-12-23 04:44:47 -0800

    error main.h fixed - add bracket at the end of function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - appends SRC to DEST, remove \0 and adds at the end
+ * _strcat - appends SRC to DEST, remove \0 an
+ * adds at the end
  *
  * @dest: varaible 1
  * @src: variable 2
@@ -12,15 +13,18 @@ char *_strcat(char *dest, char *src)
 {
 	int len = 0, replc;
 
-	while (dest[len])
+	while (dest[len]) /*get the length of dsest*/
 	{
 		len++;
 	}
 
 	for (replc = 0; src[replc] != '\0'; replc++)
+	/*add the charactors of SRC to DEST*/
 	{
 		dest[len] = src[replc];
 	}
+
+	dest[len] = '\0'; /*add special character to the end*/
 	return (dest);
 }
 
