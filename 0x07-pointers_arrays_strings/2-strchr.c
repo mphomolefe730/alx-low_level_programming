46411572b7d263332ff752869c596ebcca944ae2
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-03 14:14:04 -0800

    2_strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locate character in a string
+ *
+ * @s: the string to search in
+ * @c: character to search for
+ * Return: first occurrence of c, else null
+ **/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char null = '\0';
+
+	/*for (i = 0; s[i] <= c; i++)*/
+	/*{*/
+		/*if (s[i] = '\0')*/
+		/*{*/
+			/*return (null);*/
+			/*break;*/
+		/*}*/
+		/*else if (s[i] == c)*/
+			/*return (i);*/
+		/*else*/
+			/*continue;*/
+	/*}*/
+
+	for (i = 0; s != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (i);
+	}
+	if (s[i] == '\0')
+		return (null);
+}
