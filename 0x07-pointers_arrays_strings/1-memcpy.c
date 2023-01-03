32233a77c1388a74aa02d8cda69de4b03cbb3e82
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-03 12:28:37 -0800

    1-memcpy, a program that copies another string

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - copies n byter from src to des
+ *
+ * @dest: the destination pointer/string
+ * @src: the source pointer/string
+ * @n: the size of the string
+ *
+ * Return: dest
+ **/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
