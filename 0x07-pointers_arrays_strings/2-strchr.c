77894dbfbf490a0eac362d48f314fbf0d745f736
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-04 03:49:45 -0800

    2-strchr using function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,34 +11,11 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-	char null = '\0';
+	char *i;
 
-	/*for (i = 0; s[i] <= c; i++)*/
-	/*{*/
-		/*if (s[i] = '\0')*/
-		/*{*/
-			/*return (null);*/
-			/*break;*/
-		/*}*/
-		/*else if (s[i] == c)*/
-			/*return (i);*/
-		/*else*/
-			/*continue;*/
-	/*}*/
+	strchr(s, c);
 
-	/*for (i = 0; s != '\0'; i++)*/
-	/*{*/
-		/*if (s[i] == c)*/
-			/*return (i);*/
-	/*}*/
-	/*if (s[i] == '\0')*/
-		/*return (null);*/
+	i = &c;
 
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+	return (i);
 }
