1b222d9cb293f7ae17cf14e18e7f1188c58fdb98
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-04 14:09:51 -0800

    manual fnction 5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,19 +2,34 @@
 #include <string.h>
 
 /**
- * main - check the code
+ * _strstr - locates a substring
+ * @haystack: The string its looking in
+ * @needle: The string to find
  *
- * Return: Always 0 (success)
+ * Return: returna  point to the string or null if not found
  **/
 
-int main(void)
+char *_strstr(char *haystack, char *needle)
 {
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
+	int index_1;
+	int index_2;
 
-	*t = *_strpbrk(s, f);
-	_putchar(*t);
-	_putchar('\n');
-	return (0);
+	/*add to loop and which moves to next variable*/
+	for (index_1 = 0; index_1 < strlen(haystack); index_1++)
+	{
+		for (index_2 = 0; index_2 < strlen(needle); index_2++)
+		{
+			/*if haystack [0 + n]  is not equal to needle[*], break and add to loop*/
+			if (haystack[index_1 + index_2] != needle[index_2])
+				break;
+			/*if the variable are the same, return the address of haystack*/
+			if (haystack[index_1 + index_2] == needle[index_2])
+				return (&haystack[index_1]);
+			/*if none of the variables match, return NULL*/
+			if (index_2 == strlen(needle))
+			{
+				return (0);
+			}
+		}
+	}
 }
