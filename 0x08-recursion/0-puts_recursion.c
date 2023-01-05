194fd9eff583b670b4f789070c7702acf9719570
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-05 11:22:12 -0800

    Changes to be committed: 	modified:   0-puts_recursion.c 	modified:   main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,16 @@
 #include <string.h>
 
 /**
- * _put_recursion - a function that prints a string
+ * _puts_recursion - a function that prints a string
  * followed by new line
  * @s: the string input
- * Return: Always 0 (Success)
  **/
 
 void _puts_recursion(char *s)
 {
-	char  i;
+	int i;
 
-	for (i = 0, i <= strlen[s], i++)
+	for (i = 0; i <= strlen(s); i++)
 	{
 		_putchar(s[i]);
 	}
