6d6a1a54e56068b94414d0e2a667b0b7d6bf2b46
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-17 14:19:24 -0800

    arrow operator

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - initialize variable of type struct dog
@@ -11,10 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (*d == NULL)
-		*d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 
-	(*d).name = *name;
-	(*d).age = age;
-	(*d).owner = *owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
