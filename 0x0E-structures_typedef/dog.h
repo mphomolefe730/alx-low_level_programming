d33168fcc3e34d4dd33bc7950ce64c05af8b2f0a
mphomolefe730 <mphomolefe730@gmail.com>
2023-01-17 04:56:05 -0800

    start of project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a struct for dog struct
+ * @name: name of dog
+ * @age:age of dog
+ * @owner: who owners the dog
+ **/
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+#endif
